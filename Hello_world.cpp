6a718d07352c1f556ce3638cd9d06c771005cf5d
OsadchyRost <rostisl.osadchy@yandex.ru>
2024-12-23 02:13:55 +0300

    docs: add comments to the code

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
+    //Переменная для имени пользователя
     std::string name;
+    //Вводим имя пользователя
     std::cout << "Enter your name" << endl;
     std::cin >> name
+    //Выводим приветствие и имя пользователя
     std::cout << "Hello world from " << name << "!" << endl;
     return 0;
 }
